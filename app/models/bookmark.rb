fd8a90841e9c3cef294d075fc1b015e3c7b1f535
Andrew Sardone <andrew@andrewsardone.com>
2018-03-26 23:25:05 -0400

    Add Bookmark and Tag models (with has_many, :through)
    ```
    docker-compose exec web bin/rails generate model bookmark name:string url:string
    docker-compose exec web bin/rails generate model tag name:string
    docker-compose exec web bin/rails generate model tagging bookmark:belongs_to tag:belongs_to
    docker-compose run web bin/rails db:migrate
    ```

---
+++ app/models/bookmark.rb
@@ -0,0 +1,4 @@
+class Bookmark < ApplicationRecord
+  has_many :taggings
+  has_many :tags, through: :taggings
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180327032154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "bookmark_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bookmark_id"], name: "index_taggings_on_bookmark_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "taggings", "bookmarks"
+  add_foreign_key "taggings", "tags"
 end
